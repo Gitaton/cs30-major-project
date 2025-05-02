57f0e9aeab60e58a79821cabb3d376ab52382f98
Gitaton <1302484@spsd.sk.ca>
2025-05-02 10:59:09 -0600

    Added comments to major project

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
-// Project Title
-// Your Name
-// Date
+// Where's my water
+// Karthik Narayan
+// 1 May 2025
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+// https://www.reddit.com/r/askscience/comments/gfpow/how_accurately_can_we_model_fluid_mechanics/
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
