d1732b25d5319ee4eb2c59b525b917a2036a07e3
Gitaton <1302484@spsd.sk.ca>
2025-05-06 13:36:25 -0600

    Added TODO comments and began creating my grid

--- sketch.js
+++ sketch.js
@@ -5,8 +5,19 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//TODO
+// - FOR TERRAIN DESTRUCTION:
+// - - Create a grid with a relatively large cell size
+// - - When the destruction occurs, divide the cells into smaller cells (add resolution) to the now destroyed cells
+// - - MAYBE ALSO ADD MARCHING SQUARES FOR SMOOTH TRANSITIONS
+// - FOR FLUID DYNAMICS:
+// - - 
+
 
 // https://www.reddit.com/r/askscience/comments/gfpow/how_accurately_can_we_model_fluid_mechanics/
+
+let grid;
+let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,4 +26,8 @@ function setup() {
 function draw() {
   background(220);
   circle(mouseX, mouseY, 50);
+}
+
+function generateGrid() {
+
 }
