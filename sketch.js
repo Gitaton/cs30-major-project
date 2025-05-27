bda04c9a5fc7d7173e70c1d8ab4f050fe3bcdac3
Gitaton <1302484@spsd.sk.ca>
2025-05-27 14:16:19 -0600

    Worked on terrain destruction, I messed around with cell bounds, which could be the solution.

--- sketch.js
+++ sketch.js
@@ -69,6 +69,18 @@ function draw() { // Draw loop (updates every frame)
   matterEngine();
   water();
   displayGrid();
+
+  // DISPLAY CIRCLE AT THE BOUNDS OF A CELL FOR TESTING
+  fill("yellow");
+  circle(1175, 25, 10);
+  circle(1125, -25, 60);
+
+  if (mouseIsPressed && mouseButton === LEFT) { // Deletes all squares for testing purposes
+    for (let cell of groundCells) {
+      groundCells.splice(groundCells.indexOf(cell), 1);
+      World.remove(engine.world, cell.body);
+    }
+  }
 }
 
 function generateGridNoise(cols, rows) { // Generates the noise pattern responsible for creating the grid, then creates the grid pattern
@@ -83,7 +95,6 @@ function generateGridNoise(cols, rows) { // Generates the noise pattern responsi
 }
 
 function generateGrid() { // Generates the grid collidors
-  filter(GRAY);
   for (let y = 0; y < globalRows; y++) {
     for (let x = 0; x < globalCols; x++) {
       if (grid[y][x] === 0) {
@@ -111,8 +122,9 @@ function displayGrid() { // Renders the grid visually
 }
 
 function mousePressed() {
-  grid[Math.floor(globalRows/mouseY)][Math.floor(globalCols/mouseX)] = 0;
-  generateGrid();
+  // grid[Math.floor(mouseY/globalRows)][Math.floor(mouseX/globalCols)] = 0;
+  // console.log(Math.floor(mouseY/globalRows), Math.floor(mouseX/globalCols));
+  //generateGrid();
   // WIP 
   // I NEED IT TO DELETE THE PREVIOUS GRID COLLIDORS THEN MAKE A NEW ONE
 }
